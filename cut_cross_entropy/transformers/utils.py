af5f96a78c0352b5c9ece87a4daae9bfb9e97a22
Erik Wijmans <ewijmans@apple.com>
2024-11-13 15:04:12 -0500

    First commit

---
+++ cut_cross_entropy/transformers/utils.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2024 Apple Inc. All Rights Reserved.
+from dataclasses import dataclass
+from typing import TypeVar
+
+import transformers
+
+TransformersModelT = TypeVar("TransformersModelT", bound=transformers.PreTrainedModel)
+
+
+@dataclass
+class PatchOptions:
+    impl: str
+    reduction: str

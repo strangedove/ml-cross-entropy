4f365c67083216bd9f3a8a1130333cbac61470fd
Toaster <138728559+strangedove@users.noreply.github.com>
2025-03-25 19:35:11 -0600

    Update gemma3.py

--- cut_cross_entropy/transformers/gemma3.py
+++ cut_cross_entropy/transformers/gemma3.py
@@ -106,7 +106,7 @@ def cce_forward(
 
     if _PATCH_OPTS is not None and _PATCH_OPTS.use_lce(labels, self.training):
         assert labels is not None
-        loss = apply_lce(hidden_states, self.embed_tokens.weight, labels, _PATCH_OPTS, **loss_kwargs)
+        loss = apply_lce(hidden_states, self.lm_head.weight, labels, _PATCH_OPTS, **loss_kwargs)
     else:
         # Only compute necessary logits, and do not upcast them to float if we are not computing the loss
         logits = self.embed_tokens(hidden_states[:, -num_logits_to_keep:, :])
@@ -272,11 +272,11 @@ def multimodal_cce_forward(
 
     if _PATCH_OPTS is not None and _PATCH_OPTS.use_lce(labels, self.training):
         assert labels is not None
-        loss = apply_lce(hidden_states, self.embed_tokens.weight, labels, _PATCH_OPTS, **loss_kwargs)
+        loss = apply_lce(hidden_states, self.language_model.lm_head.weight, labels, _PATCH_OPTS, **loss_kwargs)
     else:
         if labels is not None:
             # Upcast to float if we need to compute the loss to avoid potential precision issues
-            logits = self.embed_tokens(hidden_states[:, -logits_to_keep, :])
+            logits = self.language_model.lm_head(hidden_states[:, -logits_to_keep, :])
             logits = logits.float()
             shift_logits = logits[..., :-1, :]
             shift_labels = labels[..., 1:]

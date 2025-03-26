bf9885f58064515720071aa3c7fc91bd8b391df5
Toaster <138728559+strangedove@users.noreply.github.com>
2025-03-25 19:36:36 -0600

    Update gemma3.py

--- cut_cross_entropy/transformers/gemma3.py
+++ cut_cross_entropy/transformers/gemma3.py
@@ -109,7 +109,7 @@ def cce_forward(
         loss = apply_lce(hidden_states, self.lm_head.weight, labels, _PATCH_OPTS, **loss_kwargs)
     else:
         # Only compute necessary logits, and do not upcast them to float if we are not computing the loss
-        logits = self.embed_tokens(hidden_states[:, -num_logits_to_keep:, :])
+        logits = self.lm_head(hidden_states[:, -num_logits_to_keep:, :])
         # In Gemma3 the `final_logit_softcapping` defaults to None, this will not activate (however, I am leaving it in as a softcapping value can be optionally applied in theory)
         if self.config.final_logit_softcapping is not None:
             logits = logits / self.config.final_logit_softcapping
